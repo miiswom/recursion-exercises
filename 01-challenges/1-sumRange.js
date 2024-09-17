a398f4b60433bee327487bdca8d6a537bd5169ca
Farmata Kane <kane.farmata@gmail.com>
2024-09-17 11:42:45 +0100

    first commit

--- 01-challenges/1-sumRange.js
+++ 01-challenges/1-sumRange.js
@@ -20,4 +20,26 @@ function sumRange(num){
 }
 */
 
+
 console.log(sumRange(3))
+
+/* --- Question 1: Sum all numbers ---
+
+Write a function called sumRange. It will take a number and return the sum of all numbers from 1 up to the number passed in.
+
+Sample: sumRange(3) returns 6, since 1 + 2 + 3 = 6.
+*/
+
+function sumRangeTwo(num, sum = 1) {
+  if(num === 1) return sum;
+
+  return sumRangeTwo(num -1, sum + num)
+}
+
+
+// base: if sum <= 1 return num
+// recursive step = sumRangeTwo(num - 1)
+// recursive call: sum += num
+
+console.log("sumRangeTwo()")
+console.log(sumRangeTwo(3))
